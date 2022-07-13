72ae91ca850ca3bad44996ee05b3195af60cd159
haraldpaaske <107993798+haraldpaaske@users.noreply.github.com>
2022-07-13 13:42:19 +0200

    added files
    files added

---
+++ js.js
@@ -0,0 +1,30 @@
+
+
+function shit(){
+     var x = document.getElementById("otherRole");
+    if(x.style.visibility == "visible"){
+        x.style.visibility = "hidden";
+    } else {
+        x.style.visibility = "visible"
+    }
+
+    x.style.display = "inline";
+    console.log(x.style.visibility);
+
+
+}
+
+function shit2(){
+    var x = document.getElementById("otherGame");
+   if(x.style.visibility == "visible"){
+       x.style.visibility = "hidden";
+   } else {
+       x.style.visibility = "visible"
+   }
+
+   x.style.display = "inline";
+   console.log(x.style.visibility);
+
+
+}
+
